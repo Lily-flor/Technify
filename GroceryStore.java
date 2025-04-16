c9410607a61e5a7843b87b59c4c5db5f657433fa
Aleo-Fiwa Mothusi Ntau <aleo.ntau@gmail.com>
2025-04-16 15:55:28 +0200

    Under construction GroceryStore.java

--- GroceryStore.java
+++ GroceryStore.java
@@ -1,12 +1,2 @@
 public class GroceryStore extends Store {
-  protected ArrayList<Product> foods;
-
-  public GroceryStore(String name, String owner, String location) {
-    super(name, owner, location);
-    foods = new ArrayList<Product> ();
-  }
-
-  public void addFood(Product p) {
-    foods.add(p);
-  }
 }
