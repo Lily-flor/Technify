daa7fc4b6c838036793387c9a985a628b703da31
KAIYO <kaiyoumi8@gmail.com>
2025-04-16 16:24:18 +0200

    Updated ClothingStore.java

--- ClothingStore.java
+++ ClothingStore.java
@@ -1,3 +1,23 @@
 public class ClothingStore extends Store{
-  private
+  private int size;
+  private String brand;
+
+
+  public ClothingStore(String name,String owner,int size,String brand){
+    super(name,owner);
+    this.size = size;
+    this.brand = brand;
+  }
+  public int getSize(){
+    return size;
+  }
+  public String getBrand(){
+    return brand;
+  }  
+  public void setSize(int newSize){
+    this.size = newSize;
+  }
+  public void setBrand(String newBrand){
+  this.brand = newBrand;
+  }
 }
