828e7151f2ef4f6163ed21ed92679e2fbb9faddf
seanthecoder07 <lesiapetoangelica@gmail.com>
2025-04-16 15:45:19 +0200

    Update Store.java

--- Store.java
+++ Store.java
@@ -1,14 +1,18 @@
+import java.util.ArrayList;
+
 public class Store {
   //fields
   protected String name;
   protected String owner;
   protected String location;
+  protected Product product;
 
   //constructor
   public Store(String name, String owner, String location) {
     this.name = name;
     this.owner = owner;
     this.location = location;
+    ArrayList<Product> list = new ArrayList<>();
   }
   //instance methods
   //basic setters
